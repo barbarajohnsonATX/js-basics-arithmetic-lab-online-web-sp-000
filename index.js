51662f430bede753cbfba07e2ebc3e4ef463ee34
Barbara Johnson <johnson.barbara@gmail.com>
2019-08-18 04:51:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ Variables defined in the test suite:
 
 You should NOT try to redefine any of the above variables.
 */
- let newID = oldID + 100000
  
+ const newID = oldID + 1000000000 ;
+
 // Write your code below this line!
  
