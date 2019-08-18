a01307889802feb611d6d15500b3561c4328880c
Barbara Johnson <johnson.barbara@gmail.com>
2019-08-18 05:05:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,5 @@ const newID = oldID + 1000000000 ;
 const ageIsValid = Number.isInteger(currentAge);
 const randomNumber = Math.random() * 20;
 const randomInteger = Math.floor(randomNumber);
+const randomUserID = Math.random()*20 + 1000000001;
+
